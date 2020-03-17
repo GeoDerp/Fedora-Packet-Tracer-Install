543a9650d7dc32d64ad58eccbd5bea8a5df3d0e2
john whately <18461782+GeoDerp@users.noreply.github.com>
2020-03-17 20:33:49 +1030

    Create Install.sh
    Might not 100% work yet. Will be adjusting it when I have time

---
+++ Install.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+#Fedora Packet Tracer 7.3.0 (.deb) install yr.2020 (could work for openSUSE, but havent tested it)
+
+## configuring OS veribles
+. /etc/os-release
+if [[ $NAME = Fedora* ]]
+then ins="dnf" && os="$NAME"_"$VERSION_ID"
+elif [[ ! -z "$OSTREE_VERSION" ]] #For Silverteam
+then ins='rpm-ostree' && os="$NAME"_"$VERSION_ID"
+elif  [[ $NAME = "openSUSE Leap" ]]
+then ins="zypper" && os="$NAME"_"$VERSION_ID"
+elif  [[ $NAME = "openSUSE Tumbleweed" ]]
+then ins="zypper" && os=$NAME
+else echo "can't find distro name, assuming openSUSE" && upd="sudo zypper dup -y" && os=$NAME
+fi
+
+# Download required rpm's
+sudo $ins install qt5-qtwebkit qt5-qtsvg qt5-qtscript libpng12 git qt5-qtwayland-devel	double-conversion
+
+version=730
+
+#get file from www.netacad.com
+wget {incert .deb here}
+https://www.netacad.com/portal/resources/file/aa38a51f-45bb-4eb1-89a0-01d961ae1432
+
+#Folling code (to line 47) from [von Andreas Grupp](https://grupp-web.de/cms/2020/01/03/installation-of-packet-tracer-7-3-on-rpm-linux-systems-without-alien/)
+mkdir /tmp/PacketTracerInst
+cp "PacketTracer_"$version"_amd64.deb /tmp/PacketTracerInst"
+#rm "PacketTracer_"$version"_amd64.deb"
+cd /tmp/PacketTracerInst
+ar -xv "PacketTracer_"$version"_amd64.deb"
+mkdir control
+tar -C control -Jxf control.tar.xz
+mkdir data
+tar -C data -Jxf data.tar.xz
+cd data
+
+# remove old versions
+rm -rf /opt/pt
+rm -rf /usr/share/applications/cisco-pt7.desktop
+rm -rf /usr/share/applications/cisco-ptsa7.desktop
+rm -rf /usr/share/icons/hicolor/48x48/apps/pt7.png
+
+cp -r usr /
+cp -r opt /
+
+ln -s /usr/lib64/libdouble-conversion.so.3.1.5 /usr/lib64/libdouble-conversion.so.1
+
+# 'update icon and file assocation'
+sudo xdg-desktop-menu install /usr/share/applications/cisco-pt7.desktop
+sudo xdg-desktop-menu install /usr/share/applications/cisco-ptsa7.desktop
+sudo update-mime-database /usr/share/mime
+sudo gtk-update-icon-cache --force --ignore-theme-index /usr/share/icons/gnome
+sudo xdg-mime default cisco-ptsa7.desktop x-scheme-handler/pttp
+
+sudo ln -sf /opt/pt/packettracer /usr/local/bin/packettracer
+sudo cp /opt/pt/bin/Cisco-PacketTracer.desktop /usr/share/applications/
+
+# Add enviroment settings
+sudo bash -c 'cat >> /etc/profile <<EOL
+PT7HOME=/opt/pt
+export PT7HOME
+QT_DEVICE_PIXEL_RATIO=auto
+export QT_DEVICE_PIXEL_RATIO
+EOL'
+
+
+# install libcrypto.so.1.0.0 (source from sincorchetes on github)
+git clone https://github.com/sincorchetes/packettracer
+cd packettracer
+cp libcrypto.so.1.0.0 /opt/pt/bin/
+cd ..
+sudo rm -r packettracer
+
+# install libjpeg.so.8 (from my repo on opensusu build made by the Graphics Project (opensuse team))
+dnf config-manager --add-repo "https://download.opensuse.org/repositories/home:GeoDerp:branches:graphics/"$os"/home:GeoDerp:branches:graphics.repo"
+dnf install libjpeg-turbo #note if qt5(PacketTracer) dosnt find it, you can find libjpeg.so.8 manually and cp it over (EX. sudo cp /usr/lib/libjpeg.so.8 /opt/pt/bin/)
+
+#### add text in packettracer
+sed '/3/ a export QT_QPA_PLATFORM=\"wayland;xcb\"' /opt/pt/packettracer > /tmp/packettracer
+sudo cp /tmp/packettracer /opt/pt/packettracer && sudo rm /tmp/packettracer
